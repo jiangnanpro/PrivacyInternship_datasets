b9d6285d96138610eff0b826816ef9af09a3c10b
Rafael Munoz <rafaelmugo07@gmail.com>
2021-07-13 17:58:45 +0200

    naming changed to attacker/defender/evaluator

--- split_qmnist.py
+++ split_qmnist.py
@@ -13,29 +13,29 @@ qmnist_targets = qall.targets.numpy()
 qmnist_len = qmnist_images.shape[0]
 
 # Set the size of each partition of the split
-private_partition_ratio = 1/3
-reserved_partition_ratio = 1/3
+defender_partition_ratio = 1/3
+evaluator_partition_ratio = 1/3
 
-private_partition_size = int(qmnist_len*private_partition_ratio)+1
-reserved_partition_size = int(qmnist_len*reserved_partition_ratio)+1
+defender_partition_size = int(qmnist_len*defender_partition_ratio)+1
+evaluator_partition_size = int(qmnist_len*evaluator_partition_ratio)+1
 
 
 # Randomly selecting the images for each partition
 rng = np.random.RandomState(2021)
-random_seq = rng.choice(qmnist_len,size=private_partition_size+reserved_partition_size, replace=False)
+random_seq = rng.choice(qmnist_len,size=defender_partition_size+evaluator_partition_size, replace=False)
 
-private_partition_indexes = random_seq[0:private_partition_size]
-reserved_partition_indexes = random_seq[private_partition_size:]
-attack_partition_indexes = np.setdiff1d(np.array(range(qmnist_len)),random_seq, assume_unique=True)
+defender_partition_indexes = random_seq[0:defender_partition_size]
+evaluator_partition_indexes = random_seq[defender_partition_size:]
+attacker_partition_indexes = np.setdiff1d(np.array(range(qmnist_len)),random_seq, assume_unique=True)
 
 # Create dict of partitions
 split_dict = dict()
-split_dict['x_private'] = qmnist_images[private_partition_indexes]
-split_dict['x_attack'] = qmnist_images[attack_partition_indexes]
-split_dict['x_reserved'] = qmnist_images[reserved_partition_indexes]
-split_dict['y_private'] = qmnist_targets[private_partition_indexes]
-split_dict['y_attack'] = qmnist_targets[attack_partition_indexes]
-split_dict['y_reserved'] = qmnist_targets[reserved_partition_indexes]
+split_dict['x_defender'] = qmnist_images[defender_partition_indexes]
+split_dict['x_attacker'] = qmnist_images[attacker_partition_indexes]
+split_dict['x_evaluator'] = qmnist_images[evaluator_partition_indexes]
+split_dict['y_defender'] = qmnist_targets[defender_partition_indexes]
+split_dict['y_attacker'] = qmnist_targets[attacker_partition_indexes]
+split_dict['y_evaluator'] = qmnist_targets[evaluator_partition_indexes]
 
 # Store the dict using pickle
 with open('QMNIST.pickle', 'wb') as f:
